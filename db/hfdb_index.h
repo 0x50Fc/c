64c024615763b663789ad175ccad7aab84548934
zhang hailong <hailongz@qq.com>
2013-06-14 16:48:19 +0800

    remove hindex add hfdb_index

---
+++ db/hfdb_index.h
@@ -0,0 +1,14 @@
+//
+//  hfdb_index.h
+//  vTeam
+//
+//  Created by zhang hailong on 13-6-14.
+//  Copyright (c) 2013年 hailong.org. All rights reserved.
+//
+
+#ifndef vTeam_hfdb_index_h
+#define vTeam_hfdb_index_h
+
+
+
+#endif
